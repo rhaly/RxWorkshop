99338da02c4ecfb3bdfa3bf912c47a706f0f2915
Rafal Hały <Rafal Hay>
2016-02-16 22:53:49 +0100

    create xfapp communication pcl

---
+++ XFApp/XFApp.Communication/Serializable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace System
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Delegate, Inherited = false)]
+    [ComVisible(true)]
+    public sealed class SerializableAttribute : Attribute
+    {       
+    }
+}
